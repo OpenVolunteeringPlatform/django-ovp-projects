77f92fb97597760bfaab284428853a5dcddf5937
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-10-25 17:45:22 -0200

    Include address in project

--- ovp_projects/serializers.py
+++ ovp_projects/serializers.py
@@ -1,12 +1,27 @@
 from ovp_projects import models
 
+from ovp_core import models as core_models
+from ovp_core.serializers import GoogleAddressSerializer
+
 from rest_framework import serializers
 
 class ProjectCreateSerializer(serializers.ModelSerializer):
+  address = GoogleAddressSerializer()
+
   class Meta:
     model = models.Project
-    fields = ['id', 'name', 'slug', 'owner', 'highlighted', 'published', 'published_date', 'created_date']
+    fields = ['id', 'name', 'slug', 'owner', 'details', 'description', 'highlighted', 'published', 'published_date', 'created_date', 'address']
     read_only_fields = ['highlighted', 'published', 'published_date', 'created_date']
+
+  def create(self, validated_data):
+    address_data = validated_data.pop('address')
+    address_sr = GoogleAddressSerializer(data=address_data)
+    address_sr.is_valid(raise_exception=True)
+    address = address_sr.create(address_data)
+
+    validated_data['address'] = address
+    project = models.Project.objects.create(**validated_data)
+    return project
 
 class ProjectSearchSerializer(serializers.ModelSerializer):
   class Meta:
--- ovp_projects/views.py
+++ ovp_projects/views.py
@@ -6,23 +6,27 @@ from ovp_users import models as users_models
 from rest_framework import mixins
 from rest_framework import viewsets
 from rest_framework import response
+from rest_framework import status
 
-class ProjectResourceViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
+class ProjectResourceViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
   """
   ProjectResourceViewSet resource endpoint
   """
   queryset = models.Project.objects.all()
+  lookup_field = 'slug'
+  lookup_value_regex = '[^/]+' # default is [^/.]+ - here we're allowing dots in the url slug field
 
   def get_serializer_class(self):
+    #if self.action == 'create':
     return serializers.ProjectCreateSerializer
 
   def create(self, request, *args, **kwargs):
-    #request.data.owner = request.user.pk
     user = users_models.User.objects.all().first()
     request.data['owner'] = user.pk
 
     serializer = self.get_serializer(data=request.data)
     serializer.is_valid(raise_exception=True)
-    model = self.perform_create(serializer)
+    serializer.save()
+
     headers = self.get_success_headers(serializer.data)
     return response.Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
