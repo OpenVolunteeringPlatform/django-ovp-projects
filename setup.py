28bcc00f06512dec16fd37a71488265d2c5beae7
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-11-13 18:22:45 -0200

    Upgrade ovp packages

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ setup(
       'python-dateutil>=2.5.3,<2.6.0',
       'codecov>=2.0.5,<2.1.0',
       'coverage>=4.2,<4.3.0',
-      'ovp-core>=0.1.4,<1.0.0',
-      'ovp-uploads>=0.1.3,<1.0.0',
-      'ovp-organizations>=0.1.1,<1.0.0',
+      'ovp-core>=1.0.0,<1.1.0',
+      'ovp-uploads>=1.0.0,<1.1.0',
+      'ovp-organizations>=0.1.2,<1.0.0',
     ]
 )
