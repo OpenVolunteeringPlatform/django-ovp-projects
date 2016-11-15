431f874fc2b2499e07dd56fef2432f0273db1754
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-11-15 16:43:18 -0200

    Add permissions.IsAuthenticated to ProjectResource.create()

--- ovp_projects/views.py
+++ ovp_projects/views.py
@@ -5,6 +5,7 @@ from ovp_users import models as users_models
 
 from rest_framework import mixins
 from rest_framework import viewsets
+from rest_framework import permissions
 from rest_framework import response
 from rest_framework import status
 
@@ -16,11 +17,6 @@ class ProjectResourceViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin,
   lookup_field = 'slug'
   lookup_value_regex = '[^/]+' # default is [^/.]+ - here we're allowing dots in the url slug field
 
-  def get_serializer_class(self):
-    if self.action == 'create':
-      return serializers.ProjectCreateSerializer
-    return serializers.ProjectRetrieveSerializer
-
   def create(self, request, *args, **kwargs):
     user = users_models.User.objects.all().first()
     request.data['owner'] = user.pk
@@ -31,3 +27,19 @@ class ProjectResourceViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin,
 
     headers = self.get_success_headers(serializer.data)
     return response.Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+  # We need to override get_permissions and get_serializer_class to work
+  # with multiple serializers and permissions
+  def get_permissions(self):
+    request = self.get_serializer_context()['request']
+    if self.action == 'create':
+      self.permission_classes = (permissions.IsAuthenticated, )
+    elif self.action in ['retrieve']:
+      self.permission_classes = ()
+
+    return super(ProjectResourceViewSet, self).get_permissions()
+
+  def get_serializer_class(self):
+    if self.action == 'create':
+      return serializers.ProjectCreateSerializer
+    return serializers.ProjectRetrieveSerializer
