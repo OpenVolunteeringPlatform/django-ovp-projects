47efa93e3902400ac7832f3bd5b7e1061ced2995
Cidiomar Jr <cidiomar.jr@gmail.com>
2017-01-02 20:10:31 -0200

    update entity formating and job/jobdate relation

--- ovp_projects/models/job.py
+++ ovp_projects/models/job.py
@@ -2,9 +2,15 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 class JobDate(models.Model):
-  name = models.CharField(_("Start date"), blank=True, null=True, max_length=20)
+  name = models.CharField(_("Label"), blank=True, null=True, max_length=20)
   start_date = models.DateTimeField(_("Start date"))
   end_date = models.DateTimeField(_("End date"))
+  job = models.ForeignKey('Job', on_delete=models.CASCADE, blank=True, null=True, related_name='dates')
+
+  def __str__(self):
+    start_date = self.start_date and self.start_date.strftime("%d/%m/%Y %T") or '#'
+    end_date = self.end_date and self.end_date.strftime("%d/%m/%Y %T") or '#'
+    return "{} : {} ~ {}".format(self.name, start_date, end_date)
 
   class Meta:
     app_label = 'ovp_projects'
@@ -17,10 +23,12 @@ class Job(models.Model):
   start_date = models.DateTimeField(_("Start date"), blank=True, null=True)
   end_date = models.DateTimeField(_("End date"), blank=True, null=True)
   can_be_done_remotely = models.BooleanField(_("This job can be done remotely"), default=False)
-  dates = models.ManyToManyField('JobDate', blank=True)
 
   def __str__(self):
-    return "{} - {}".format(self.start_date, self.end_date)
+    name = self.project and self.project.name or _('Unbound Job')
+    start_date = self.start_date and self.start_date.strftime("%d/%m/%Y") or ''
+    end_date = self.end_date and self.end_date.strftime("%d/%m/%Y") or ''
+    return "{} : {} ~ {}".format(name, start_date, end_date)
 
   def update_dates(self):
     start = self.dates.all().order_by('start_date').first().start_date
@@ -33,3 +41,4 @@ class Job(models.Model):
     app_label = 'ovp_projects'
     verbose_name = _('job')
     verbose_name_plural = _('jobs')
+
