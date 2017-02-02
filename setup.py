32e20d39c497bf782a3d55db414f1ccc3f1de2a8
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-02-02 14:55:19 -0200

    Add drf-nested-routers as dependency

--- setup.py
+++ setup.py
@@ -27,5 +27,8 @@ setup(
       'ovp-core>=1.0.0,<2.0.0',
       'ovp-uploads>=1.0.0,<2.0.0',
       'ovp-organizations>=1.0.0,<2.0.0',
+    ],
+    dependency_links = [
+      'git+https://github.com/alanjds/drf-nested-routers.git@0438289'
     ]
 )
