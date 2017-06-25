176835f70ec1453401dab98b9438bcf0b0f39efb
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-06-25 14:32:36 -0300

    Make work description nullable

--- ovp_projects/models/work.py
+++ ovp_projects/models/work.py
@@ -5,7 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 class Work(models.Model):
   project = models.OneToOneField('Project', blank=True, null=True)
   weekly_hours = models.PositiveSmallIntegerField(_('Weekly hours'), blank=True, null=True)
-  description = models.CharField(_('Description'), max_length=4000)
+  description = models.CharField(_('Description'), max_length=4000, blank=True, null=True)
   can_be_done_remotely = models.BooleanField(_('This job can be done remotely'), default=False)
 
   def __str__(self):
